971ad210e17935e8877509acf65ddadb5b043242
Celso Annes <celsoannes@gmail.com>
2025-03-03 14:17:16 -0300

    Cálculo do Custo de Produção ajustado e novas colunas adicionadas

--- views/detalhes_produto.php
+++ views/detalhes_produto.php
@@ -28,28 +28,24 @@ $stmt_pecas = $pdo->prepare("
            p.quantidade_material, p.tempo_impressao, i.Marca AS marca_impressora, i.Modelo AS modelo_impressora, 
            i.Tipo AS tipo_impressora, i.Localizacao AS localizacao_impressora, i.kWh AS consumo_impressora,
            t.Prestadora, t.kWh AS kWh_energia, t.ICMS, t.PIS_PASEP, t.COFINS, t.TOTAL_horas,
-           i.Valor_do_Bem, i.Tempo_de_Vida_Util
+           i.Valor_do_Bem, i.Tempo_de_Vida_Util, pr.lucro AS lucro_produto
     FROM pecas p
     JOIN impressoras i ON p.impressora = i.ID
     JOIN produtos_pecas pp ON p.id = pp.peca_id
     JOIN tabela_energia t ON 1=1
+    JOIN produtos pr ON pp.produto_id = pr.id  -- Garantir que o lucro do produto seja recuperado
     WHERE pp.produto_id = ?
 ");
 $stmt_pecas->execute([$id]);
 $pecas = $stmt_pecas->fetchAll();
 
+
 // Função para calcular o custo de energia
 function calcularCustoEnergia($consumo_impressora, $tempo_impressao, $kWh_energia, $ICMS, $PIS_PASEP, $COFINS, $TOTAL_horas) {
-    // Converter o tempo de impressão (hh:mm:ss) para minutos
     list($horas, $minutos, $segundos) = explode(":", $tempo_impressao);
     $tempo_impressao_minutos = ($horas * 60) + $minutos + ($segundos / 60);
-    
-    // Calcular o custo do kWh com base na prestadora
     $custo_kWh = $TOTAL_horas + ($TOTAL_horas * ($ICMS / 100)) + ($TOTAL_horas * ($PIS_PASEP / 100)) + ($TOTAL_horas * ($COFINS / 100));
-    
-    // Cálculo do custo de energia
-    $custo_energia = ($consumo_impressora * $tempo_impressao_minutos / 60) * $custo_kWh;
-    return $custo_energia;
+    return ($consumo_impressora * $tempo_impressao_minutos / 60) * $custo_kWh;
 }
 
 // Função para calcular o custo por quilo do material
@@ -68,20 +64,41 @@ function calcularCustoMaterial($tipo_impressora, $material_peca, $quantidade_mat
         $valor_kg = $resina['Valor_Kg'];
     }
     
-    // Calcular o custo por grama e converter para o custo total
-    $custo_material = ($valor_kg / 1000) * $quantidade_material;
-    return $custo_material;
+    return ($valor_kg / 1000) * $quantidade_material;
 }
 
 // Função para calcular a depreciação/manutenção
 function calcularDepreciacao($tempo_impressao, $Valor_do_Bem, $Tempo_de_Vida_Util) {
-    // Converter o tempo de impressão (hh:mm:ss) para minutos
     list($horas, $minutos, $segundos) = explode(":", $tempo_impressao);
     $tempo_impressao_minutos = ($horas * 60) + $minutos + ($segundos / 60);
+    return ($Valor_do_Bem / ($Tempo_de_Vida_Util * 60)) * $tempo_impressao_minutos;
+}
+
+// Cálculo do custo de produção e lucro
+// Cálculo do custo de produção e lucro
+function calcularCustoProducao($peca) {
+    $custo_energia = calcularCustoEnergia($peca['consumo_impressora'], $peca['tempo_impressao'], $peca['kWh_energia'], 
+                                          $peca['ICMS'], $peca['PIS_PASEP'], $peca['COFINS'], $peca['TOTAL_horas']);
+    $custo_material = calcularCustoMaterial($peca['tipo_impressora'], $peca['material_peca'], $peca['quantidade_material']);
+    $depreciacao = calcularDepreciacao($peca['tempo_impressao'], $peca['Valor_do_Bem'], $peca['Tempo_de_Vida_Util']);
     
-    // Calcular a depreciação
-    $depreciacao = ($Valor_do_Bem / ($Tempo_de_Vida_Util * 60)) * $tempo_impressao_minutos;
-    return $depreciacao;
+    // Cálculo do custo de produção
+    $custo_producao = $custo_energia + $custo_material + $depreciacao;
+    
+    // Cálculo do valor de venda com lucro
+    $valor_venda = $custo_producao + ($custo_producao * ($peca['lucro_produto'] / 100));
+    
+    // Cálculo do lucro
+    $lucro = $valor_venda - $custo_producao;
+    
+    return [
+        'custo_energia' => $custo_energia,
+        'custo_material' => $custo_material,
+        'depreciacao' => $depreciacao,
+        'custo_producao' => $custo_producao,
+        'valor_venda' => $valor_venda,
+        'lucro' => $lucro
+    ];
 }
 ?>
 
@@ -103,16 +120,13 @@ function calcularDepreciacao($tempo_impressao, $Valor_do_Bem, $Tempo_de_Vida_Uti
             <li class="list-group-item"><strong>Download:</strong> <a href="<?= htmlspecialchars($produto['baixar']) ?>" target="_blank">Baixar</a></li>
             <li class="list-group-item"><strong>Observações:</strong> <?= nl2br(htmlspecialchars($produto['observacoes'])) ?></li>
             <li class="list-group-item"><strong>Lucro Estimado:</strong> R$ <?= number_format($produto['lucro'], 2, ',', '.') ?></li>
+            
         </ul>
 
         <h3 class="mt-4">Detalhes da Peça Associada</h3>
         <ul class="list-group">
             <?php foreach ($pecas as $peca): 
-                // Cálculos de custo
-                $custo_energia = calcularCustoEnergia($peca['consumo_impressora'], $peca['tempo_impressao'], $peca['kWh_energia'], 
-                                                      $peca['ICMS'], $peca['PIS_PASEP'], $peca['COFINS'], $peca['TOTAL_horas']);
-                $custo_material = calcularCustoMaterial($peca['tipo_impressora'], $peca['material_peca'], $peca['quantidade_material']);
-                $depreciacao = calcularDepreciacao($peca['tempo_impressao'], $peca['Valor_do_Bem'], $peca['Tempo_de_Vida_Util']);
+                $custos = calcularCustoProducao($peca);
             ?>
                 <li class="list-group-item">
                     <img src="<?= htmlspecialchars($peca['imagem_peca']) ?>" alt="Imagem da Peça" class="img-fluid mb-2" style="max-width: 100px;">
@@ -142,21 +156,23 @@ function calcularDepreciacao($tempo_impressao, $Valor_do_Bem, $Tempo_de_Vida_Uti
                     <th>Custo de Energia (R$)</th>
                     <th>Custo de Material (R$)</th>
                     <th>Depreciação/Manutenção (R$)</th>
+                    <th>Custo de Produção (R$)</th>
+                    <th>Lucro (R$)</th>
+                    <th>Valor de Venda (R$)</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($pecas as $peca): 
-                    // Recalcular os custos para exibição
-                    $custo_energia = calcularCustoEnergia($peca['consumo_impressora'], $peca['tempo_impressao'], $peca['kWh_energia'], 
-                                                          $peca['ICMS'], $peca['PIS_PASEP'], $peca['COFINS'], $peca['TOTAL_horas']);
-                    $custo_material = calcularCustoMaterial($peca['tipo_impressora'], $peca['material_peca'], $peca['quantidade_material']);
-                    $depreciacao = calcularDepreciacao($peca['tempo_impressao'], $peca['Valor_do_Bem'], $peca['Tempo_de_Vida_Util']);
+                    $custos = calcularCustoProducao($peca);
                 ?>
                 <tr>
                     <td><?= htmlspecialchars($peca['nome_peca']) ?></td>
-                    <td>R$ <?= number_format($custo_energia, 2, ',', '.') ?></td>
-                    <td>R$ <?= number_format($custo_material, 2, ',', '.') ?></td>
-                    <td>R$ <?= number_format($depreciacao, 2, ',', '.') ?></td>
+                    <td>R$ <?= number_format($custos['custo_energia'], 2, ',', '.') ?></td>
+                    <td>R$ <?= number_format($custos['custo_material'], 2, ',', '.') ?></td>
+                    <td>R$ <?= number_format($custos['depreciacao'], 2, ',', '.') ?></td>
+                    <td>R$ <?= number_format($custos['custo_producao'], 2, ',', '.') ?></td>
+                    <td>R$ <?= number_format($custos['lucro'], 2, ',', '.') ?></td>
+                    <td>R$ <?= number_format($custos['valor_venda'], 2, ',', '.') ?></td>
                 </tr>
                 <?php endforeach; ?>
             </tbody>
