69f464ed85de0bb621302e3dfafeaea3b5b44c01
Celso Annes <celsoannes@gmail.com>
2025-03-04 01:23:58 -0300

    Menu fixado sempre ao topo da pagina

--- includes/menu.php
+++ includes/menu.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="estilo.css"> <!-- Seu arquivo CSS -->
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
         <a class="navbar-brand" href="index.php">Gestão 3D</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
